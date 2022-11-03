df12b72db4ce68a7a2ca3d4f02749c8bb06fa348
Somogyvári Benedek <s.benedek2003@gmail.com>
2022-11-03 14:25:00 +0100

    Basic window and a few util functions

---
+++ Engine/src/Platform/Window.cpp
@@ -0,0 +1,83 @@
+#include "Window.h"
+
+namespace Cobalt
+{
+	Window::Window(const WindowProperties& properties)
+	{
+		m_Properties = properties;
+	}
+
+	bool Window::Create()
+	{
+		if (!glfwInit()) return false;
+
+		m_Window = glfwCreateWindow(m_Properties.Width, m_Properties.Height, m_Properties.Title.c_str(), nullptr, nullptr);
+		if (m_Window == nullptr) return false;
+
+		glfwMakeContextCurrent(m_Window);
+
+		/* Callbacks */
+
+		glfwSetFramebufferSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+		{
+			glViewport(0, 0, width, height);
+		});
+
+		const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		int sys_width = mode->width;
+		int sys_height = mode->height;
+		glfwSetWindowMonitor(m_Window, nullptr, 
+			(sys_width / 2) - (m_Properties.Width / 2),
+			(sys_height / 2) - (m_Properties.Height / 2),
+			m_Properties.Width, m_Properties.Height, GLFW_DONT_CARE);
+
+		return true;
+	}
+
+	WindowProperties Window::GetProperties()
+	{
+		int width, height;
+		glfwGetFramebufferSize(m_Window, &width, &height);
+		
+		m_Properties.Width = width;
+		m_Properties.Height = height;
+
+		return m_Properties;
+	}
+
+	void Window::SetWidth(uint32_t width)
+	{
+		m_Properties.Width = width;
+		glfwSetWindowSize(m_Window, m_Properties.Width, m_Properties.Height);
+		glViewport(0, 0, m_Properties.Width, m_Properties.Height);
+	}
+
+	void Window::SetHeight(uint32_t height)
+	{
+		m_Properties.Height = height;
+		glfwSetWindowSize(m_Window, m_Properties.Width, m_Properties.Height);
+		glViewport(0, 0, m_Properties.Width, m_Properties.Height);
+	}
+
+	void Window::SetTitle(const std::string& title)
+	{
+		m_Properties.Title = title;
+		glfwSetWindowTitle(m_Window, m_Properties.Title.c_str());
+	}
+
+	void Window::SetVsync(bool value)
+	{
+		m_Properties.Vsync = value;
+		glfwSwapInterval((int)value);
+	}
+
+	void Window::Destroy()
+	{
+		glfwDestroyWindow(m_Window);
+		glfwTerminate();
+	}
+
+	void Window::FrameBuffer_Size_Callback(GLFWwindow* window, int width, int height)
+	{
+	}
+}
---
+++ Engine/src/Platform/Window.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "GLFW/glfw3.h"
+#include <string>
+
+namespace Cobalt
+{
+	struct WindowProperties
+	{
+		std::string Title;
+		uint32_t Width;
+		uint32_t Height;
+		bool Vsync;
+
+		WindowProperties(const std::string& title = "Cobalt Engine", uint32_t width = 1280, uint32_t height = 720, bool vsync = true)
+			: Title(title), Width(width), Height(height), Vsync(vsync) {}
+	};
+
+	class Window
+	{
+	public:
+		Window(const WindowProperties& properties = WindowProperties());
+		bool Create();
+
+		GLFWwindow* GetHandle() const { return m_Window; }
+		WindowProperties GetProperties();
+
+		void SetWidth(uint32_t width);
+		void SetHeight(uint32_t height);
+		void SetTitle(const std::string& title);
+
+		void SetVsync(bool value);
+
+		void Destroy();
+
+	private:
+		GLFWwindow* m_Window;
+		WindowProperties m_Properties;
+
+		void FrameBuffer_Size_Callback(GLFWwindow* window, int width, int height);
+	};
+}
