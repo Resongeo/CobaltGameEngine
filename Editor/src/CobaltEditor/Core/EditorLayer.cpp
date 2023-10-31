83affd6feb57f0e5d727d8b6298b5f15a7750ebc
Somogyvári Benedek <s.benedek2003@gmail.com>
2023-10-31 16:14:02 +0100

    Changed base font
    Also added an icon font

--- Editor/src/CobaltEditor/Core/EditorLayer.cpp
+++ Editor/src/CobaltEditor/Core/EditorLayer.cpp
@@ -5,12 +5,46 @@
 #include "CobaltEditor/Panels/MainViewportPanel.h"
 #include "CobaltEditor/Panels/SceneHierarchyPanel.h"
 
+#include "CobaltEditor/Core/Codepoints.h"
+
 namespace CobaltEditor
 {
 	void EditorLayer::OnAttach()
 	{
 		ImGuiIO& io = ImGui::GetIO();
 		io.IniFilename = "..\\imgui.ini";
+
+		float baseFontSize = 18.0f;
+		float iconFontSize = baseFontSize * 2.0f / 3.0f;
+
+		ImFontConfig fontConfig;
+		fontConfig.MergeMode = true;
+		fontConfig.PixelSnapH = true;
+		fontConfig.GlyphMinAdvanceX = iconFontSize;
+		fontConfig.GlyphOffset = { 0, 2 };
+		static const ImWchar icon_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
+
+		io.FontDefault = io.Fonts->AddFontFromFileTTF("..\\assets\\fonts\\JetBrainsMono-Regular.ttf", baseFontSize);
+		io.Fonts->AddFontFromFileTTF("..\\assets\\fonts\\" FONT_ICON_FILE_NAME_FAS, baseFontSize, &fontConfig, icon_ranges);
+
+		/*
+		
+		float baseFontSize = 20.0f; // TODO: Get values from config
+		float iconFontSize = baseFontSize * 2.0f / 3.0f;
+
+		ImFontConfig config;
+		config.MergeMode = true;
+		config.PixelSnapH = true;
+		config.GlyphMinAdvanceX = iconFontSize;
+		config.GlyphOffset = { 0, 2 };
+		static const ImWchar icon_ranges[] = { ICON_MIN, ICON_MAX, 0 };
+
+		m_EditorFonts.Regular = io.Fonts->AddFontFromFileTTF("..\\assets\\fonts\\JetBrainsMono-Regular.ttf", baseFontSize);
+		io.Fonts->AddFontFromFileTTF("..\\assets\\fonts\\forkawesome-webfont.ttf", baseFontSize, &config, icon_ranges);
+
+		m_EditorFonts.SemiBold = io.Fonts->AddFontFromFileTTF("..\\assets\\fonts\\JetBrainsMono-Bold.ttf", baseFontSize);
+		io.Fonts->AddFontFromFileTTF("..\\assets\\fonts\\forkawesome-webfont.ttf", baseFontSize, &config, icon_ranges);
+		*/
 
 		m_Window = Application::GetWindow();
 		m_Scene = CreateRef<Scene>("Scene");
